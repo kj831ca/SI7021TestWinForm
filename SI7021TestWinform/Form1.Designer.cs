6f251df13f87b515066f05e1d6d95a3c92e619e3
KRIS JEARAKUL <34535680+kj831ca@users.noreply.github.com>
2019-01-08 16:10:15 -0800

    Add files via upload

---
+++ SI7021TestWinform/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace SI7021TestWinform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.toolStripContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(40, 47);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(133, 63);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // toolStripContainer1
+            // 
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(8, 0);
+            this.toolStripContainer1.Location = new System.Drawing.Point(411, 291);
+            this.toolStripContainer1.Name = "toolStripContainer1";
+            this.toolStripContainer1.Size = new System.Drawing.Size(8, 8);
+            this.toolStripContainer1.TabIndex = 2;
+            this.toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Location = new System.Drawing.Point(210, 47);
+            this.zedGraphControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(650, 350);
+            this.zedGraphControl1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(971, 596);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Controls.Add(this.toolStripContainer1);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStripContainer1.ResumeLayout(false);
+            this.toolStripContainer1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+    }
+}
+
---
+++ SI7021TestWinform/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+using ZedGraph;
+using SI7021Module;
+
+namespace SI7021TestWinform
+{
+    public partial class Form1 : Form
+    {
+        System.Timers.Timer pollingTimers;
+
+		Random rnd;
+        RollingPointPairList temperatureSeries;
+        RollingPointPairList humiditySeries;
+		SI7021Sensor _siSensor;
+        public Form1()
+        {
+            InitializeComponent();
+            pollingTimers = new System.Timers.Timer();
+            pollingTimers.Interval = 200;
+            pollingTimers.Elapsed += PollingTimers_Elapsed;
+
+			rnd = new Random();
+			_siSensor = new SI7021Sensor();
+            initGraph();
+        }
+
+        private void initGraph()
+        {
+            temperatureSeries = new RollingPointPairList(300);
+            humiditySeries = new RollingPointPairList(300);
+
+            zedGraphControl1.GraphPane.Title.Text = "Temperature/Humidity Chart with SI7021 sensors";
+			zedGraphControl1.GraphPane.XAxis.Type = AxisType.Date;
+			zedGraphControl1.GraphPane.XAxis.Scale.Format = "HH:mm:ss";
+            zedGraphControl1.GraphPane.AddCurve("Temperature", temperatureSeries, Color.Green,SymbolType.None);
+			zedGraphControl1.GraphPane.AddCurve("Humidity", humiditySeries, Color.Blue,SymbolType.None);
+            updateAxis();
+
+        }
+
+        private delegate void UpdateAxis();
+        private void updateAxis()
+        {
+            if(zedGraphControl1.InvokeRequired)
+            {
+                this.Invoke(new UpdateAxis(updateAxis));
+                return;
+            }
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
+        }
+        private void clearGraph()
+        {
+            temperatureSeries.Clear();
+            humiditySeries.Clear();
+        }
+        private void updateChart(double temperature, double humidity)
+        {
+            XDate x = new XDate(DateTime.Now);
+
+            temperatureSeries.Add(x, temperature);
+            humiditySeries.Add(x, humidity);
+            updateAxis();
+        }
+
+        private void PollingTimers_Elapsed(object sender, ElapsedEventArgs e)
+        {
+			double t = _siSensor.ReadTemp();
+			double h = _siSensor.ReadRH();
+			updateChart(t,h);
+            
+        }
+        bool isStart = false;
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (!isStart)
+            {
+                clearGraph();
+                pollingTimers.Start();
+                buttonStart.Text = "Stop";
+            }
+            else
+            {
+                pollingTimers.Stop();
+                buttonStart.Text = "Start";
+            }
+            isStart = !isStart;
+
+        }
+    }
+}
